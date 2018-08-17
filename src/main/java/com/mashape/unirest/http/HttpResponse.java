8da776e4e1b817f037221660bcadfcf94531ff5b
IYanagimachi <isao.yanagimachi@hpe.com>
2018-08-17 13:16:24 -0400

    OOM workaround on inputstream

--- src/main/java/com/mashape/unirest/http/HttpResponse.java
+++ src/main/java/com/mashape/unirest/http/HttpResponse.java
@@ -31,106 +31,143 @@ import com.mashape.unirest.http.utils.ResponseUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.StatusLine;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.util.EntityUtils;
 
 import java.io.ByteArrayInputStream;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.GZIPInputStream;
 
-public class HttpResponse<T> {
+public class HttpResponse<T> implements Closeable {
 
-	private int statusCode;
-	private String statusText;
-	private Headers headers = new Headers();
-	private InputStream rawBody;
-	private T body;
+    private int statusCode;
+    private String statusText;
+    private Headers headers = new Headers();
+    private InputStream rawBody;
+    private T body;
+    private File ofile;
+    private HttpEntity responseEntity;
+    private HttpRequestBase requestObj;
 
-	@SuppressWarnings("unchecked")
-	public HttpResponse(org.apache.http.HttpResponse response, Class<T> responseClass) {
-		HttpEntity responseEntity = response.getEntity();
-		ObjectMapper objectMapper = (ObjectMapper) Options.getOption(Option.OBJECT_MAPPER);
+    @SuppressWarnings("unchecked")
+    public HttpResponse(org.apache.http.HttpResponse response, Class<T> responseClass) {
+        this(response, responseClass, null);
+    }
+    @SuppressWarnings("unchecked")
+    public HttpResponse(org.apache.http.HttpResponse response, Class<T> responseClass, HttpRequestBase requestObj) {
+        responseEntity = response.getEntity();
+        this.requestObj = requestObj;
+        ObjectMapper objectMapper = (ObjectMapper) Options.getOption(Option.OBJECT_MAPPER);
 
-		Header[] allHeaders = response.getAllHeaders();
-		for (Header header : allHeaders) {
-			String headerName = header.getName();
-			List<String> list = headers.get(headerName);
-			if (list == null)
-				list = new ArrayList<String>();
-			list.add(header.getValue());
-			headers.put(headerName, list);
-		}
-		StatusLine statusLine = response.getStatusLine();
-		this.statusCode = statusLine.getStatusCode();
-		this.statusText = statusLine.getReasonPhrase();
+        Header[] allHeaders = response.getAllHeaders();
+        for (Header header : allHeaders) {
+            String headerName = header.getName();
+            List<String> list = headers.get(headerName);
+            if (list == null)
+                list = new ArrayList<String>();
+            list.add(header.getValue());
+            headers.put(headerName, list);
+        }
+        StatusLine statusLine = response.getStatusLine();
+        this.statusCode = statusLine.getStatusCode();
+        this.statusText = statusLine.getReasonPhrase();
 
-		if (responseEntity != null) {
-			String charset = "UTF-8";
+        if (responseEntity != null) {
+            String charset = "UTF-8";
 
-			Header contentType = responseEntity.getContentType();
-			if (contentType != null) {
-				String responseCharset = ResponseUtils.getCharsetFromContentType(contentType.getValue());
-				if (responseCharset != null && !responseCharset.trim().equals("")) {
-					charset = responseCharset;
-				}
-			}
+            Header contentType = responseEntity.getContentType();
+            if (contentType != null) {
+                String responseCharset = ResponseUtils.getCharsetFromContentType(contentType.getValue());
+                if (responseCharset != null && !responseCharset.trim().equals("")) {
+                    charset = responseCharset;
+                }
+            }
 
-			try {
-				byte[] rawBody;
-				try {
-					InputStream responseInputStream = responseEntity.getContent();
-					if (ResponseUtils.isGzipped(responseEntity.getContentEncoding())) {
-						responseInputStream = new GZIPInputStream(responseEntity.getContent());
-					}
-					rawBody = ResponseUtils.getBytes(responseInputStream);
-				} catch (IOException e2) {
-					throw new RuntimeException(e2);
-				}
-				this.rawBody = new ByteArrayInputStream(rawBody);
+            try {
+                byte[] rawBody = null;
+                try {
+                    InputStream responseInputStream = responseEntity.getContent();
+                    if (ResponseUtils.isGzipped(responseEntity.getContentEncoding())) {
+                        responseInputStream = new GZIPInputStream(responseEntity.getContent());
+                    }
+                    //If responseClass is inputStream, just copy the stream as it is to
+                    //rawBody. Then require the client to explicitly close the connection using "close" method.
+                    //Otherwise the existing code will allocate a big byte buffer to download
+                    //all the contents at once.
+                    //
+                    if (InputStream.class.equals(responseClass)) {
+                        this.rawBody =  responseInputStream;
+                    } else {
+                        rawBody = ResponseUtils.getBytes(responseInputStream);
+                        this.rawBody = new ByteArrayInputStream(rawBody);
+                    }
+                } catch (IOException e2) {
+                    throw new RuntimeException(e2);
+                }
 
-				if (JsonNode.class.equals(responseClass)) {
-					String jsonString = new String(rawBody, charset).trim();
-					this.body = (T) new JsonNode(jsonString);
-				} else if (String.class.equals(responseClass)) {
-					this.body = (T) new String(rawBody, charset);
-				} else if (InputStream.class.equals(responseClass)) {
-					this.body = (T) this.rawBody;
-				} else if (objectMapper != null) {
-					this.body = objectMapper.readValue(new String(rawBody, charset), responseClass);
-				} else {
-					throw new Exception("Only String, JsonNode and InputStream are supported, or an ObjectMapper implementation is required.");
-				}
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		}
-		
-		EntityUtils.consumeQuietly(responseEntity);
-	}
+                if (JsonNode.class.equals(responseClass)) {
+                    String jsonString = new String(rawBody, charset).trim();
+                    this.body = (T) new JsonNode(jsonString);
+                } else if (String.class.equals(responseClass)) {
+                    this.body = (T) new String(rawBody, charset);
+                } else if (InputStream.class.equals(responseClass)) {
+                    this.body = (T) this.rawBody;
+                } else if (objectMapper != null) {
+                    this.body = objectMapper.readValue(new String(rawBody, charset), responseClass);
+                } else {
+                    throw new Exception("Only String, JsonNode and InputStream are supported, or an ObjectMapper implementation is required.");
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
 
-	public int getStatus() {
-		return statusCode;
-	}
+        //InputSteam client needs to call "close" method explicitly
+        //to close the connection and empty out the response entity.
+        //Otherwise, inputStream will be closed prematurely.
+        //
+        if (!InputStream.class.equals(responseClass))
+            EntityUtils.consumeQuietly(responseEntity);
+    }
 
-	public String getStatusText() {
-		return statusText;
-	}
+    public int getStatus() {
+        return statusCode;
+    }
 
-	/**
-	 * @return Response Headers (map) with <b>same case</b> as server response.
-	 * For instance use <code>getHeaders().getFirst("Location")</code> and not <code>getHeaders().getFirst("location")</code> to get first header "Location"
-	 */
-	public Headers getHeaders() {
-		return headers;
-	}
+    public String getStatusText() {
+        return statusText;
+    }
 
-	public InputStream getRawBody() {
-		return rawBody;
-	}
+    /**
+     * @return Response Headers (map) with <b>same case</b> as server response.
+     * For instance use <code>getHeaders().getFirst("Location")</code> and not <code>getHeaders().getFirst("location")</code> to get first header "Location"
+     */
+    public Headers getHeaders() {
+        return headers;
+    }
 
-	public T getBody() {
-		return body;
-	}
+    public InputStream getRawBody() {
+        return rawBody;
+    }
+
+    public T getBody() {
+        return body;
+    }
+
+    @Override
+    public void close() throws IOException {
+        //InputSteam client needs to call this method explicitly
+        //to close the connection and empty out the response entity.
+        //
+        requestObj.releaseConnection();
+        EntityUtils.consume(responseEntity);
+    }
 }
